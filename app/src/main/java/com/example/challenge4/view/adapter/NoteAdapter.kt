7524950934c4d81b5ad6e45bbe7680c744249f95
wildanfatah22 <awildanfatahillah@gmail.com>
2023-10-24 16:48:30 +0700

    layouting mainactivity and item

---
+++ app/src/main/java/com/example/challenge4/view/adapter/NoteAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.challenge4.view.adapter
+
+class NoteAdapter {
+}
--- app/src/main/java/com/example/challenge4/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/challenge4/viewmodel/MainViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.challenge4.view
+package com.example.challenge4.viewmodel
 
 import android.app.Application
 import android.content.SharedPreferences
