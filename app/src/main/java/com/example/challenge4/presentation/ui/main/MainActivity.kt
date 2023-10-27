7d5a3fb6584034b30c6f66fe13d802f0c0460fe1
wildanfatah22 <awildanfatahillah@gmail.com>
2023-10-27 17:35:57 +0700

    add logout done!

--- app/src/main/java/com/example/challenge4/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/challenge4/presentation/ui/main/MainActivity.kt
@@ -3,15 +3,18 @@ package com.example.challenge4.presentation.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.challenge4.R
 import com.example.challenge4.databinding.ActivityMainBinding
 import com.example.challenge4.domain.model.Note
 import com.example.challenge4.presentation.adapter.NoteAdapter
 import com.example.challenge4.presentation.ui.addnote.AddNoteActivity
+import com.example.challenge4.presentation.ui.auth.login.LoginActivity
 import com.example.challenge4.presentation.ui.editnote.EditNoteActivity
 
 
@@ -30,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        setSupportActionBar(binding.toolbar)
         setUpViews()
     }
 
@@ -80,5 +83,49 @@ class MainActivity : AppCompatActivity() {
     private fun visibilityEmptyData(boolean: Boolean) {
         binding.noData.visibility = if (boolean) View.VISIBLE else View.GONE
         binding.rvNote.visibility = if (boolean) View.GONE else View.VISIBLE
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.logout -> {
+                showLogoutConfirmationDialog()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showLogoutConfirmationDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Log out Confirmation")
+        builder.setMessage("Are you sure you want to log out?")
+        builder.setPositiveButton("Yes") { dialog, which ->
+            performLogout()
+        }
+        builder.setNegativeButton("Cancel") { dialog, which ->
+            dialog.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun performLogout() {
+        clearUserLoginState()
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finishAffinity()
+    }
+
+    private fun clearUserLoginState() {
+        val sharedPreferences = getSharedPreferences("isUserLogin", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("isUserLogin", false)
+        editor.apply()
     }
 }
